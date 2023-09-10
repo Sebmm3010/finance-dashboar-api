24bc96567e3c6090c03d58e45e8e42454672cf33
Sebastian madero <rockmadero@gmail.com>
2023-09-09 20:05:30 -0500

    typescript

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+/* Configuracion */
+dotenv.config();
+const app = express();
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());

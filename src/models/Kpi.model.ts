925b05ffb02a3dfecfb20ddd826cc64642f94b64
Sebastian madero <rockmadero@gmail.com>
2023-09-11 15:02:02 -0500

    kpi model db

---
+++ src/models/Kpi.model.ts
@@ -0,0 +1,95 @@
+import mongoose, { Model, Schema, model } from "mongoose";
+import { loadType } from "mongoose-currency";
+import { IKpi } from "../interfaces";
+
+loadType(mongoose);
+
+const monthlyDataSchema = new Schema(
+  {
+    month: String,
+    revenue: {
+      // @ts-ignore
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v: any) => v / 100,
+    },
+    expenses: {
+      // @ts-ignore
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v: any) => v / 100,
+    },
+    operationalExpenses: {
+      // @ts-ignore
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v: any) => v / 100,
+    },
+    nonOperationalExpenses: {
+      // @ts-ignore
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v: any) => v / 100,
+    },
+  },
+  { toJSON: { getters: true } }
+);
+
+const dailyDataSchema = new Schema(
+  {
+    day: String,
+    revenue: {
+      // @ts-ignore
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v: any) => v / 100,
+    },
+    expenses: {
+      // @ts-ignore
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v: any) => v / 100,
+    },
+  },
+  { toJSON: { getters: true } }
+);
+
+const KpiSchema = new Schema(
+  {
+    totalProfit: {
+      // @ts-ignore
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v: any) => v / 100,
+    },
+    totalRevenue: {
+      // @ts-ignore
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v: any) => v / 100,
+    },
+    totalExpenses: {
+      // @ts-ignore
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v: any) => v / 100,
+    },
+    exprensesByCategory: {
+      type: Map,
+      currency: "USD",
+      of: {
+        // @ts-ignore
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v: any) => v / 100,
+      },
+      monthlyData: [monthlyDataSchema],
+      dailyData: [dailyDataSchema],
+    },
+  },
+  { toJSON: { getters: true }, timestamps: true }
+);
+
+const Kpi: Model<IKpi> = mongoose.models.Kpi || model("Kpi", KpiSchema);
+
+export default Kpi;
